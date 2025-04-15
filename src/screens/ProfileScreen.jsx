c1ad59103f116beefcdcf4feed59379fff45e5d5
franciscoL0pez <franlopez.2001@hotmail.com>
2025-04-15 18:31:11 -0300

    Refactor logOut buttom

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -105,12 +105,23 @@ const styles = StyleSheet.create({
   },
 
     logoutButton: {
-      padding: 10,
-         alignItems: 'center',
+      paddingVertical: 8,
+      paddingHorizontal: 16,
+      backgroundColor: '#FF6B6B',
+      borderRadius: 20,
+      alignSelf: 'flex-end',
+      marginRight: 16,
+      marginTop: 8,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+      elevation: 3,
     },
     logoutButtonText: {
-      fontSize: 16,
-      color: 'red',
+      fontSize: 14,
+      color: 'white',
+      fontWeight: 'bold',
     },
 
   title: {
